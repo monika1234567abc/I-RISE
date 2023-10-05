664b91993b1f89d0c03d857d4e8caeb5570984ae
monika1234567abc <144302291+monika1234567abc@users.noreply.github.com>
2023-10-05 23:33:04 +0530

    Day-2.1.js

---
+++ Day-2.js/Day-2.1.js
@@ -0,0 +1,15 @@
+var no=2
+
+if (no>25){
+
+    console.log ("No.is gretter than 5")
+}else if(no<25){
+
+    console.log ("No.is less than 5")
+}else if (no==25) {
+
+    console.log ("No.is equal")
+}else {
+
+    console.log ("No.no match")
+}
---
+++ Day-2.js/Day-2.2.js
@@ -0,0 +1,18 @@
+var age=17
+
+if (age <18){
+
+    console.log ("You are not eligible for driving license")
+
+}else if (age<19 && age >80){
+
+    console.log ("You are eligible for driving license")
+}else if (age <0 || age==0){ 
+
+    console.log ("You just born now ,you are not eligible")
+
+}else {
+
+    console.log("You are over age not eligible")
+}
+
