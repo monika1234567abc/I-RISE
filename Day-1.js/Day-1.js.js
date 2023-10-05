58e734d4c1c27f23d939da4a066bb5e6a0c21eb2
monika1234567abc <144302291+monika1234567abc@users.noreply.github.com>
2023-10-05 18:00:51 +0530

    Day-1.js

---
+++ Day-1.js/Day-1.js.js
@@ -0,0 +1,27 @@
+console.log ("Monika Jadhav")
+console.log ("ENTC ENGG.")
+console.log ("Passing Year 2015")
+console.log ("Amravati Univercity")
+console.log("5 Year of experience")
+var Prarthana =123456789
+console.log (Prarthana)
+var Irise= "hello All"
+console.log (Irise)
+var Num1 = 56;
+var Num2 = 24;
+var Addition=Num1+Num2;
+console.log (Addition,"-Addition")
+var Substraction=Num1-Num2;
+console.log (Substraction,"-Substraction")
+var Multiplication=Num1*Num2;
+console.log (Multiplication,"-Multiplication")
+var Division=Num1/Num2;
+console.log (Division,"-Division")
+var Module=Num1%Num2;
+console.log (Module,"-Module")
+
+
+
+
+
+
