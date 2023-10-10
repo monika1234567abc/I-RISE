8c07b54a23e63008c9c75888837bad4030fe4bc9
monika1234567abc <144302291+monika1234567abc@users.noreply.github.com>
2023-10-11 02:10:59 +0530

    day-4.1.js

---
+++ Day-4.js/Day-4.1.js.js
@@ -0,0 +1,18 @@
+function arithmatic (Number1,Number2){
+
+    console.log (Number1+Number2,"-Addition")
+    console.log (Number1-Number2,"-Sub")
+    console.log (Number1*Number2,"-Mul")
+    console.log (Number1/Number2,"-Divide")
+}
+arithmatic (4,4);
+
+function divid (no1,no2){
+
+    var dividation=no1/no2;
+    console.log (dividation)
+    console.log (Math.floor (dividation))
+    console.log (Math.ceil (dividation))
+}
+
+divid (5,3);
