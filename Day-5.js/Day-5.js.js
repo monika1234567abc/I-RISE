548e282b82cf327538488f37adf5def378e75175
monika1234567abc <144302291+monika1234567abc@users.noreply.github.com>
2023-10-11 22:42:57 +0530

    Day-5.js

---
+++ Day-5.js/Day-5.js.js
@@ -0,0 +1,20 @@
+const StudentList=["Prarthana", "Pooja", "Monika","Namrata","Ashwini","Neha"]
+console.log (StudentList);
+
+var list=["a","b","c","d","e","f"]
+console.log (list);
+console.log(list[0]);
+console.log(list[1]);
+console.log(list[2]);
+console.log(list[3]);
+console.log(list[4]);
+console.log(list[5]);
+console.log(list.length)
+
+
+Students=["Prarthana", "Pooja", "Monika","Namrata","Ashwini","Neha"]
+for (i=0;i<Students.length;i++){
+
+    console.log(i);
+
+}
